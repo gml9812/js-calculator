a2d09e18d9db2b3db1c493818e7c87a7b5594ae9
gml9812 <hee.jun.yun@gmail.com>
2021-11-15 23:45:07 +0900

    test: AC 버튼 누르면 0으로 초기화

--- cypress/integration/calc.spec.js
+++ cypress/integration/calc.spec.js
@@ -7,4 +7,11 @@ describe('test counter', () => {
     cy.get('.digits').contains('1').click();
     cy.get('#total').should('have.text', '1');
   });
+
+  it('clears total when AC pushed', () => {
+    cy.get('.digits').contains('1').click();
+    cy.get('.digits').contains('5').click();
+    cy.get('.modifier').click();
+    cy.get('#total').should('have.value', '0');
+  });
 });
